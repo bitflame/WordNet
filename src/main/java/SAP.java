615686c000cbe5f84b0d938281fbbe644ea013ac
bitflame <monazzam@protonmail.com>
2022-01-21 16:49:27 -0500

    updated the code to separate minDistance calculation duties from ancestor calculation duties and cache the last value for each. Need to test and debug it and see how it works.

--- src/main/java/SAP.java
+++ src/main/java/SAP.java
@@ -2,7 +2,6 @@ import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.DirectedCycle;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Stack;
 
 public class SAP {
     private boolean hasCycle = false;
@@ -21,8 +20,8 @@ public class SAP {
     private static final int INFINITY = Integer.MAX_VALUE;
     private boolean fromPathLoop = false;
     private boolean toPathLoop = false;
-    private boolean fromBridge = false;
-    private boolean toBridge = false;
+    private int lengthSource;
+    private int lengthDestination;
 
     // constructor takes a digraph ( not necessarily a DAG )
     public SAP(Digraph digraph) {
@@ -38,12 +37,22 @@ public class SAP {
 
     // length of the shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        if (v == from && w == to) return minDistance;
+        if (v == lengthSource && w == lengthDestination) return minDistance;
         if (v == w) {
-            ancestor = v;
             return minDistance = 0;
         }
-        ancestor(v, w);
+        if ((digraphDFCopy.indegree(from) == 0 && digraphDFCopy.outdegree(from) == 0) || (digraphDFCopy.indegree(to) == 0 &&
+                digraphDFCopy.outdegree(to) == 0)) return minDistance;
+        fromPathLoop = false;
+        toPathLoop = false;
+        n = digraphDFCopy.V();
+        fromMarked = new boolean[n];
+        toMarked = new boolean[n];
+        fromEdgeTo = new int[n];
+        toEdgeTo = new int[n];
+        fromDistTo = new int[n];
+        toDistTo = new int[n];
+        calculateMinDistance(v, w);
         return minDistance;
     }
 
@@ -51,8 +60,21 @@ public class SAP {
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
         if (v == null || w == null)
             throw new IllegalArgumentException("Iterable value to SAP.length() can not be null.");
-
-        ancestor(v, w);
+        int distance = INFINITY;
+        // System.out.printf("sap triggers ancestor() with iterables ");
+        for (int i : v) {
+            for (int j : w) {
+                // StdOut.printf("Calling ancestor(%d, %d) ", i, j);
+                if (i == lengthSource && j == lengthDestination) return minDistance;
+                else if (i == 0 && j == 0) {
+                    minDistance = calculateMinDistance(i, j);
+                } else {
+                    distance = calculateMinDistance(i, j);
+                    if (distance < minDistance) minDistance = distance;
+                }
+            }
+        }
+        minDistance = distance;
         return minDistance;
     }
 
@@ -60,11 +82,9 @@ public class SAP {
     public int ancestor(int v, int w) {
         if (this.from == v && this.to == w) return ancestor;
         if (v == w) {
-            minDistance = 0;
             return ancestor = v;
         }
         ancestor = -1;
-        minDistance = -1;
         this.from = v;
         this.to = w;
         if ((digraphDFCopy.indegree(from) == 0 && digraphDFCopy.outdegree(from) == 0) || (digraphDFCopy.indegree(to) == 0 &&
@@ -87,44 +107,64 @@ public class SAP {
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         if (v == null || w == null)
             throw new IllegalArgumentException("Iterable value to SAP.ancestor() can not be null.");
-        int distance = Integer.MAX_VALUE;
-        int currentAncestor = -1;
-        // System.out.printf("sap triggers ancestor() with iterables ");
         for (int i : v) {
             for (int j : w) {
-                // StdOut.printf("Calling ancestor(%d, %d) ", i, j);
-                ancestor(i, j);
-                if (distance > minDistance) {
-                    distance = minDistance;
-                    currentAncestor = ancestor;
+                lockStepBFS(i, j);
+            }
+        }
+        return ancestor;
+    }
+
+    private int calculateMinDistance(int f, int t) {
+        int currentDistance = INFINITY;
+        if (f != lengthSource && t != lengthDestination) {
+            lengthSource = f;
+            lengthDestination = t;
+            Queue<Integer> fromQueue = new Queue<>();
+            Queue<Integer> toQueue = new Queue<>();
+            fromQueue.enqueue(f);
+            toQueue.enqueue(t);
+            fromMarked[f] = true;
+            fromDistTo[f] = 0;
+            toMarked[t] = true;
+            toDistTo[t] = 0;
+            int w = -1;
+            int v = -1;
+            while (!(fromPathLoop && toPathLoop)) {
+                if (fromQueue.isEmpty() && toQueue.isEmpty()) break;
+                if (!fromQueue.isEmpty()) v = fromQueue.dequeue();
+                if (!toQueue.isEmpty()) w = toQueue.dequeue();
+                for (int j : digraphDFCopy.adj(v)) {
+                    if (fromMarked[j]) {
+                        fromPathLoop = true;
+                    } else if (!toMarked[j]) {
+                        fromEdgeTo[j] = v;
+                        fromMarked[j] = true;
+                        fromDistTo[j] = fromDistTo[v] + 1;
+                        fromQueue.enqueue(j);
+                    } else {
+                        currentDistance = Math.min(currentDistance, toDistTo[j] + fromDistTo[v] + 1);
+                    }
+                }
+            }
+            for (int k : digraphDFCopy.adj(w)) {
+                if (toMarked[k]) {
+                    toPathLoop = true;
+                } else if (!fromMarked[k]) {
+                    toEdgeTo[k] = w;
+                    toMarked[k] = true;
+                    toDistTo[k] = toDistTo[w] + 1;
+                    toQueue.enqueue(k);
+                } else {
+                    currentDistance = Math.min(currentDistance, fromDistTo[k] + toDistTo[w] + 1);
                 }
             }
         }
-        minDistance = distance;
-        ancestor = currentAncestor;
-        return ancestor;
-    }
-
-    private int getAncestorII(int from, int to) {
-        if (this.from == from && this.to == to) return ancestor;
-        if (from == to) {
-            minDistance = 0;
-            return ancestor = from;
+        if (currentDistance == 0) currentDistance = 1;
+        else if (currentDistance == INFINITY) {
+            currentDistance = -1;
         }
-        ancestor = -1;
-        minDistance = -1;
-        this.from = from;
-        this.to = to;
-        n = digraphDFCopy.V();
-        lockStepBFS(from, to);
-        return ancestor;
-    }
-
-    private int getLengthII(int f, int t) {
-        if (f != from && t != to) {
-            getAncestorII(f, t);
-        }
-        return minDistance;
+        return currentDistance;
     }
 
     private void lockStepBFS(int f, int t) {
@@ -138,63 +178,38 @@ public class SAP {
         toDistTo[t] = 0;
         int w = -1;
         int v = -1;
-        minDistance = INFINITY;
-        while ((!fromQueue.isEmpty() && !toQueue.isEmpty()) || (fromPathLoop && toPathLoop)) {
+        while (!(fromPathLoop && toPathLoop)) {
+            if (fromQueue.isEmpty() && toQueue.isEmpty()) break;
             if (!fromQueue.isEmpty()) v = fromQueue.dequeue();
             if (!toQueue.isEmpty()) w = toQueue.dequeue();
             for (int j : digraphDFCopy.adj(v)) {
                 // keep going until you hit a loop in both queues, or you run out of nodes to process
                 if (fromMarked[j]) {
-                    // you are in a self loop
-                    ancestor = -1;
-                    minDistance = -1;
+                    // in a self loop
                     fromPathLoop = true;
                 } else if (!toMarked[j]) {
                     fromEdgeTo[j] = v;
                     fromMarked[j] = true;
                     fromDistTo[j] = fromDistTo[v] + 1;
                     fromQueue.enqueue(j);
-                } else if (toEdgeTo[j] != w) {
-                    // you hit one of the other guys previously traversed nodes
-                    System.out.println("Found a j with abnormal edgeTo. source is: " + f + " destination is: " + t);
-                    if (minDistance > 1 + fromDistTo[v]) {
-                        System.out.println("Updating minDistance for j with abnormal edgeTo.");
-                        ancestor = j;
-                        minDistance = fromDistTo[v] + 1 + toDistTo[j];
-                    }
-                    fromBridge = true;
                 } else {
                     ancestor = j;
-                    minDistance = Math.min(minDistance, toDistTo[j] + fromDistTo[v] + 1);
                 }
             }
 
             for (int k : digraphDFCopy.adj(w)) {
                 if (toMarked[k]) {
-                    // you are in a self loop
-                    ancestor = -1;
-                    minDistance = -1;
                     toPathLoop = true;
                 } else if (!fromMarked[k]) {
                     toEdgeTo[k] = w;
                     toMarked[k] = true;
                     toDistTo[k] = toDistTo[w] + 1;
                     toQueue.enqueue(k);
-                } else if (fromEdgeTo[k] != v && fromEdgeTo[k] != 0) {
-                    System.out.println("Found a k with abnormal edgeTo. source is: " + f + " destination is: " + t);
-                    // only update the ancestor if you have a shorter minDistance
-                    if (minDistance > (toDistTo[toEdgeTo[k]] + 1 + fromDistTo[v] + 1)) {
-                        System.out.println("Updating minDistance for k with abnormal edgeTo.");
-                        ancestor = k;
-                        minDistance = fromDistTo[k] + toDistTo[w] + 1;
-                    }
                 } else {
                     ancestor = k;
-                    minDistance = Math.min(minDistance, fromDistTo[k] + toDistTo[w] + 1);
                 }
             }
         }
-        if (minDistance == 0) minDistance = 1;
     }
 
     public static void main(String[] args) {
--- src/test/java/AutoGraderTests.java
+++ src/test/java/AutoGraderTests.java
@@ -4,10 +4,23 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class AutoGraderTests {
     public static void main(String[] args) {
-        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph3----------------------------------");
-        Digraph digraph = new Digraph(new In("digraph3.txt"));
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 1 ----------------------------------");
+        Digraph digraph = new Digraph(new In("digraph1.txt"));
         SAP sap = new SAP(digraph);
-        int distance = sap.length(10, 7);
+        int distance = sap.length(3, 3);
+        if (distance != 0) throw new AssertionError("Distance between 3, and 3 should be 0, but it is: " + distance);
+        int ancestor = sap.ancestor(3, 3);
+        if (ancestor != 3)
+            throw new AssertionError("The value of ancestor between 3 and 3 should be 3, but it is: " + ancestor);
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 2 ----------------------------------");
+        digraph = new Digraph(new In("digraph2.txt"));
+        sap = new SAP(digraph);
+        distance = sap.length(4, 1);
+        if (distance != 3) throw new AssertionError("Distance between 4, and 1 should be 3, but it is: " + distance);
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 3 ----------------------------------");
+        digraph = new Digraph(new In("digraph3.txt"));
+        sap = new SAP(digraph);
+        distance = sap.length(10, 7);
         if (distance != 3) throw new AssertionError("Distance between 10 and 7 should be 3, but it is: " + distance);
         distance = sap.length(5, 14);
         if (distance != -1)
@@ -18,7 +31,7 @@ public class AutoGraderTests {
         distance = sap.length(10, 3);
         if (distance != -1)
             throw new AssertionError("Distance between 10 and 3 should be -1 since the two nodes are not connected, but it is: " + distance);
-        int ancestor = sap.ancestor(7, 11);
+        ancestor = sap.ancestor(7, 11);
         if (ancestor != 8)
             throw new AssertionError("The value of ancestor between 7 and 11 should be 8, but it is: " + ancestor);
         distance = sap.length(7, 11);
@@ -32,26 +45,46 @@ public class AutoGraderTests {
         distance = sap.length(12, 13);
         if (distance != 4)
             throw new AssertionError("The distance between 12 and 13 should be 4, but it is: " + distance);
-        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph4----------------------------------");
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 4 ----------------------------------");
         digraph = new Digraph(new In("digraph4.txt"));
         sap = new SAP(digraph);
         distance = sap.length(1, 4);
         if (distance != 3) throw new AssertionError("The distance between 1 and 4 should be 3, but it is: " + distance);
-        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph5----------------------------------");
+        distance = sap.length(4, 1);
+        if (distance != 3) throw new AssertionError("The distance between 4 and 1 should be 3, but it is: " + distance);
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 5 ----------------------------------");
         digraph = new Digraph(new In("digraph5.txt"));
         sap = new SAP(digraph);
         distance = sap.length(17, 21);
         if (distance != 5)
             throw new AssertionError("The distance between 17 and 21 should be 5, but it is: " + distance);
-        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph6----------------------------------");
+        distance = sap.length(14, 21);
+        if (distance != 8)
+            throw new AssertionError("The distance between 14 and 21 should be 8, but it is: " + distance);
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 6 ----------------------------------");
         digraph = new Digraph(new In("digraph6.txt"));
         sap = new SAP(digraph);
         distance = sap.length(0, 5);
         if (distance != 5) throw new AssertionError("The distance between 0 and 5 should be 5, but it is: " + distance);
-        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph9----------------------------------");
+        distance = sap.length(7, 3);
+        if (distance != 1) throw new AssertionError("The distance between 7 and 3 should be 1, but it is: " + distance);
+        ancestor = sap.ancestor(7, 3);
+        if (ancestor != 3)
+            throw new AssertionError("The ancestor  between 7 and 3 should be 3, but it is: " + ancestor);
+        distance = sap.length(7, 7);
+        if (distance != 0)
+            throw new AssertionError("The distance between 7, and 7 should be 0, but it is: " + distance);
+        StdOut.println("----------------------------------Running AutoGrader Tests for Digraph 9 ----------------------------------");
         digraph = new Digraph(new In("digraph9.txt"));
         sap = new SAP(digraph);
         distance = sap.length(7, 8);
-        if (distance != -1) throw new AssertionError("The distance between 7 and 8 should be -1, but it is: " + distance);
+        if (distance != -1)
+            throw new AssertionError("The distance between 7 and 8 should be -1, but it is: " + distance);
+        distance = sap.length(7, 4);
+        if (distance != 3)
+            throw new AssertionError("The distance between 7, and 4 should be 3, but it is: " + distance);
+        distance = sap.length(4, 0);
+        if (distance != 3)
+            throw new AssertionError("The distance between 4, and 0 should be 3, but it is: " + distance);
     }
 }
