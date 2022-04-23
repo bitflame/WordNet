16bfcd1928f6b84bfae2f72236eb1dbd63ae370e
Shahin Ansari <monazzam@protonmail.com>
2022-04-23 12:52:56 -0400

    Did some tests and the method seems to woark.

--- src/main/java/DeluxeBFS.java
+++ src/main/java/DeluxeBFS.java
@@ -158,10 +158,27 @@ public class DeluxeBFS {
         In in = new In(args[0]);
         Digraph G = new Digraph(in);
         // StdOut.println(G);
-
         int s = Integer.parseInt(args[1]);
+        s = 4;
         DeluxeBFS bfs = new DeluxeBFS(G, s);
-
+        for (int v = 0; v < G.V(); v++) {
+            if (bfs.hasPathTo(v)) {
+                StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
+                for (int x : bfs.pathTo(v)) {
+                    if (x == s) StdOut.print(x);
+                    else StdOut.print("->" + x);
+                }
+                StdOut.println();
+            } else {
+                StdOut.printf("%d to %d (-):  not connected\n", s, v);
+            }
+        }
+        Digraph digraph = new Digraph(new In("tinyDG.txt"));
+        if (digraph.equals(G)) {
+            System.out.printf("running the updated method.\n");
+            bfs.updateSource(G, s);
+        } else System.out.printf("The graphs were not equal, and did not run the update method.\n");
+        bfs.updateSource(G, 3);
         for (int v = 0; v < G.V(); v++) {
             if (bfs.hasPathTo(v)) {
                 StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
